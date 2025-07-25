40a2399d73c97a967677dae1bc4b72338560e134
AlexandreRodriguesStrapasson <alexandre.strapasson1@gmail.com>
2025-07-24 21:28:12 -0300

    ✨ initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="a4">
+    Ficha editavel
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style scoped>
+#a4 {
+  width: 210mm;
+  height: 297mm;
+
+  border: 1px solid black;
+  
+  background-color: white;
+
+  margin: 20px auto;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  
+  box-sizing: border-box; 
+  padding: 10mm;
+}
+</style>
