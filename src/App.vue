212db63513f4b6f7c180b774a79fa18e406d6bc9
AlexandreRodriguesStrapasson <alexandre.strapasson1@gmail.com>
2025-07-25 20:26:20 -0300

    ✨ add a initial text and css

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
 <template>
   <div id="a4">
-    
+    <div class="playerDataField">
+      <div class="characterName">
+        <p>Personagem:</p>
+      </div>
+      <div class="playerNameField">
+        <p>Jogador:</p>
+      </div>
+      <div class="classAndAncestryyField">
+        <p>Classe e Ancestralidade</p>
+      </div>
+      <div class="levelAndXpField">
+        <p>Nível e XP</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,6 +25,7 @@ export default {
 
 <style scoped>
 #a4 {
+  position: relative;
   width: 210mm;
   height: 297mm;
 
@@ -27,5 +41,43 @@ export default {
   
   box-sizing: border-box; 
   padding: 10mm;
+}
+.playerDataField{
+  position: absolute;
+  top: 23mm;
+  left: 24mm;
+  width: 95mm;
+  height: 21mm;
+  display: flex;
+  flex-direction: row;
+}
+
+.characterName,
+.playerNameField,
+.classAndAncestryyField,
+.levelAndXpField {
+  position: absolute;
+  width: 28mm;
+  height: 18mm;
+  margin: 0;
+  margin-top: -10px;
+}
+
+.characterName, .playerNameField, .classAndAncestryyField, .levelAndXpField {
+  text-align: center;
+}
+
+.playerNameField {
+  left: 35mm;
+}
+
+.classAndAncestryyField {
+  width: 56mm;
+  left: 70mm;
+}
+
+.levelAndXpField {
+  width: 32mm;
+  left: 135mm;
 }
 </style>
