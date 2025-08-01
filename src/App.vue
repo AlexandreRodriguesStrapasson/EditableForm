c18d405fcef71fca51953d614e8c4019741fe5e9
AlexandreRodriguesStrapasson <alexandre.strapasson1@gmail.com>
2025-08-01 18:55:58 -0300

    ✨ optimized css of equipment description fields

--- src/App.vue
+++ src/App.vue
@@ -22,60 +22,49 @@
     </div>
 
     <div class="attributePointsField">
-        <div class="strengthField">
-          <input type="text" id="strengthPoint" placeholder="20">
-        </div>
-
-        <div class="strengthFieldRight">
-          <input type="text" id="strengthPointRight" placeholder="20">
-        </div>
-
-        <div class="dexterityField">
-          <input type="text" id="dexterityPoint" placeholder="20">
-        </div>
-
-        <div class="dexterityFieldRight">
-          <input type="text" id="dexterityPointRight" placeholder="20">
-        </div>
-
-        <div class="constitutionField">
-          <input type="text" id="constitutionPoint" placeholder="20">
-        </div>
-
-        <div class="constitutionFieldRight">
-          <input type="text" id="constitutionPointRight" placeholder="20">
-        </div>
-
-        <div class="intelligenceField">
-          <input type="text" id="intelligencePoint" placeholder="20">
-        </div>
-
-        <div class="intelligenceFieldRight">
-          <input type="text" id="intelligencePointRight" placeholder="20">
-        </div>
-
-        <div class="wisdomField">
-          <input type="text" id="wisdomPoint" placeholder="20">
-        </div>
-
-        <div class="wisdomFieldRight">
-          <input type="text" id="wisdomPointRight" placeholder="20">
-        </div>
-
-        <div class="charismaField">
-          <input type="text" id="charismaPoint" placeholder="20">
-        </div>
-
-        <div class="charismaFieldRight">
-          <input type="text" id="charismaPointRight" placeholder="20">
-        </div>
+      <div class="strengthField">
+        <input type="text" id="strengthPoint" placeholder="20">
+      </div>
+      <div class="strengthFieldRight">
+        <input type="text" id="strengthPointRight" placeholder="20">
+      </div>
+      <div class="dexterityField">
+        <input type="text" id="dexterityPoint" placeholder="20">
+      </div>
+      <div class="dexterityFieldRight">
+        <input type="text" id="dexterityPointRight" placeholder="20">
+      </div>
+      <div class="constitutionField">
+        <input type="text" id="constitutionPoint" placeholder="20">
+      </div>
+      <div class="constitutionFieldRight">
+        <input type="text" id="constitutionPointRight" placeholder="20">
+      </div>
+      <div class="intelligenceField">
+        <input type="text" id="intelligencePoint" placeholder="20">
+      </div>
+      <div class="intelligenceFieldRight">
+        <input type="text" id="intelligencePointRight" placeholder="20">
+      </div>
+      <div class="wisdomField">
+        <input type="text" id="wisdomPoint" placeholder="20">
+      </div>
+      <div class="wisdomFieldRight">
+        <input type="text" id="wisdomPointRight" placeholder="20">
+      </div>
+      <div class="charismaField">
+        <input type="text" id="charismaPoint" placeholder="20">
+      </div>
+      <div class="charismaFieldRight">
+        <input type="text" id="charismaPointRight" placeholder="20">
+      </div>
     </div>
 
     <div class="statusField">
-        <input type="text" id="pvField"         placeholder="00">
-        <input type="text" id="caField"         placeholder="00">
-        <input type="text" id="levelBonusField" placeholder="00">
-        <input type="text" id="movField"        placeholder="00">
+      <input type="text" id="pvField" placeholder="00">
+      <input type="text" id="caField" placeholder="00">
+      <input type="text" id="levelBonusField" placeholder="00">
+      <input type="text" id="movField" placeholder="00">
     </div>
 
     <div class="vrmField">
@@ -85,61 +74,86 @@
     </div>
 
     <div class="weaponsFields">
-        <input type="text" id="weaponNameOne"   placeholder="Espada curta">
-        <input type="text" id="weaponNameTwo"   placeholder="2x Lança">
-        <input type="text" id="weaponNameThree" placeholder="Adaga">
-        <input type="text" id="weaponNameFour"  placeholder="Arco curto">
+      <input type="text" id="weaponNameOne"   placeholder="Espada curta">
+      <input type="text" id="weaponNameTwo"   placeholder="2x Lança">
+      <input type="text" id="weaponNameThree" placeholder="Adaga">
+      <input type="text" id="weaponNameFour"  placeholder="Arco curto">
 
-        <input type="text" id="atqWeaponOne"  placeholder="10">
-        <input type="text" id="atqWeaponTwo"  placeholder="10">
-        <input type="text" id="atqWeaponTree" placeholder="10">
-        <input type="text" id="atqWeaponFour" placeholder="10">
+      <input type="text" id="atqWeaponOne"  placeholder="10">
+      <input type="text" id="atqWeaponTwo"  placeholder="10">
+      <input type="text" id="atqWeaponTree" placeholder="10">
+      <input type="text" id="atqWeaponFour" placeholder="10">
 
-        <input type="text" id="dmgWeaponOne"  placeholder="10">
-        <input type="text" id="dmgWeaponTwo"  placeholder="10">
-        <input type="text" id="dmgWeaponTree" placeholder="10">
-        <input type="text" id="dmgWeaponFour" placeholder="10">
+      <input type="text" id="dmgWeaponOne"  placeholder="10">
+      <input type="text" id="dmgWeaponTwo"  placeholder="10">
+      <input type="text" id="dmgWeaponTree" placeholder="10">
+      <input type="text" id="dmgWeaponFour" placeholder="10">
 
-        <input type="text" id="mvnWeaponOne"  placeholder="10">
-        <input type="text" id="mvnWeaponTwo"  placeholder="10">
-        <input type="text" id="mvnWeaponTree" placeholder="10">
-        <input type="text" id="mvnWeaponFour" placeholder="10">
+      <input type="text" id="mvnWeaponOne"  placeholder="10">
+      <input type="text" id="mvnWeaponTwo"  placeholder="10">
+      <input type="text" id="mvnWeaponTree" placeholder="10">
+      <input type="text" id="mvnWeaponFour" placeholder="10">
     </div>
 
     <div class="moneyFields">
-        <input type="text" id="poField" placeholder="100"><br>
-        <input type="text" id="ppField" placeholder="100"><br>
-        <input type="text" id="pcField" placeholder="100">
+      <input type="text" id="poField" placeholder="100"><br>
+      <input type="text" id="ppField" placeholder="100"><br>
+      <input type="text" id="pcField" placeholder="100">
     </div>
 
     <div class="equipmentsField">
-      <input type="text" id="eqpRow1">
-      <input type="text" id="eqpRow2">
-      <input type="text" id="eqpRow3">
-      <input type="text" id="eqpRow4">
-      <input type="text" id="eqpRow5">
-      <input type="text" id="eqpRow6">
-      <input type="text" id="eqpRow7">
-      <input type="text" id="eqpRow8">
-      <input type="text" id="eqpRow9">
-      <input type="text" id="eqpRow10">
-      <input type="text" id="eqpRow11">
-      <input type="text" id="eqpRow12">
-      <input type="text" id="eqpRow13">
-      <input type="text" id="eqpRow14">
-      <input type="text" id="eqpRow15">
-      <input type="text" id="eqpRow16">
-      <input type="text" id="eqpRow17">
-      <input type="text" id="eqpRow18">
-      <input type="text" id="eqpRow19">
-      <input type="text" id="eqpRow20">
+      <input
+        v-for="(item, index) in equipmentList"
+        :key="index"
+        v-model="equipmentList[index]"
+        :placeholder="index === firstEmptyIndex ? '...' : ''"
+        :style="getRowStyle(index)"
+      >
     </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'App',
+<script setup>
+import { ref, computed } from 'vue'
+
+const equipmentList = ref(Array(20).fill(''))
+
+const firstEmptyIndex = computed(() =>
+  equipmentList.value.findIndex(item => item.trim() === '')
+)
+
+const getRowStyle = (index) => {
+  if (index <= 6) {
+    return { width: '70mm', marginLeft: '2mm' };
+  }
+  if (index >= 7 && index <= 9) {
+    return { width: '64mm', marginLeft: '8mm' };
+  }
+  if (index === 10 || index === 11) {
+    return { width: '66mm', marginLeft: '6mm' };
+  }
+  if (index === 12 || index === 13) {
+    return { width: '56mm', marginLeft: '16mm' };
+  }
+  if (index === 14) {
+    return { width: '69mm', marginLeft: '3mm' };
+  }
+  if (index === 15) {
+    return { width: '41mm', marginLeft: '31mm' };
+  }
+  if (index === 16) {
+    return { width: '36mm', marginLeft: '36mm' };
+  }
+  if (index === 17) {
+    return { width: '31mm', marginLeft: '41mm' };
+  }
+  if (index === 18) {
+    return { width: '28mm', marginLeft: '44mm' };
+  }
+  if (index === 19) {
+    return { width: '26mm', marginLeft: '46mm' };
+  }
+  return {};
 }
 </script>
 
@@ -148,18 +162,14 @@ export default {
   position: relative;
   width: 210mm;
   height: 297mm;
-
   border: 1px solid black;
-  
   background-image: url('/img/formModel.png');
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center center;
-
   margin: 20px auto;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-  
-  box-sizing: border-box; 
+  box-sizing: border-box;
   padding: 10mm;
 }
 .playerDataField{
@@ -251,7 +261,7 @@ export default {
   left: 23mm;
 }
 
-.strengthFieldRight, .dexterityFieldRight, .constitutionFieldRight, .intelligenceFieldRight, 
+.strengthFieldRight, .dexterityFieldRight, .constitutionFieldRight, .intelligenceFieldRight,
 .wisdomFieldRight, .charismaFieldRight{
   position: absolute;
   left: 36mm;
@@ -481,17 +491,17 @@ export default {
 #mvnWeaponOne{
   bottom: 0.7mm;
   left: 31mm;
-} 
+}
 
 #mvnWeaponTwo{
   top: 7.5mm;
   left: 12mm;
-} 
+}
 
 #mvnWeaponTree{
   top: 9.5mm;
   left: 83.5mm;
-} 
+}
 
 #mvnWeaponFour{
   top: 17mm;
@@ -526,60 +536,4 @@ export default {
   border: none;
   background-color: transparent;
 }
-
-#eqpRow1, #eqpRow2, #eqpRow3, #eqpRow4, #eqpRow5, #eqpRow6, #eqpRow7{
-  width: 70mm;
-  margin-left: 2mm;
-}
-
-#eqpRow8, #eqpRow9, #eqpRow10{
-  width: 64mm;
-  margin-left: 8mm;
-}
-
-#eqpRow11, #eqpRow12{
-  width: 66mm;
-  margin-left: 6mm;
-}
-
-#eqpRow13{
-  width: 56mm;
-  margin-left: 16mm;
-}
-
-#eqpRow14{
-  width: 56mm;
-  margin-left: 16mm;
-}
-
-#eqpRow15{
-  width: 69mm;
-  margin-left: 3mm;
-}
-
-#eqpRow16{
-  width: 41mm;
-  margin-left: 31mm;
-}
-
-#eqpRow17{
-  width: 36mm;
-  margin-left: 36mm;
-}
-
-#eqpRow18{
-  width: 31mm;
-  margin-left: 41mm;
-}
-
-#eqpRow19{
-  width: 28mm;
-  margin-left: 44mm;
-}
-
-#eqpRow20{
-  width: 26mm;
-  margin-left: 46mm;
-}
-
 </style>
